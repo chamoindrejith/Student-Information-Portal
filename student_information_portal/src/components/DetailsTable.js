f18e266291d0b5e52c5ebafeb9a976a3d73f8b4f
Chamodi Indrejith <104416340+chamoindrejith@users.noreply.github.com>
2024-11-19 17:18:13 +0000

    Add feature Details Table

---
+++ student_information_portal/src/components/DetailsTable.js
@@ -0,0 +1,27 @@
+const DetailsTable = ({students, setStudentIndex}) =>{
+    return(
+        <table border={1}>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Country</th>
+                <th>Profile</th>
+            </tr>
+            {students.map((student,i)=>(
+                <tr>
+                    <td>{student.firstName}</td>
+                    <td>{student.lastName}</td>
+                    <td>{student.course}</td>
+                    <td>{student.country}</td>
+                    <td>
+                        <button onClick={()=>setStudentIndex(i)}> View
+                        </button>
+                    </td>
+                </tr>
+            ))}
+        </table>
+    )
+}
+
+export default Table
