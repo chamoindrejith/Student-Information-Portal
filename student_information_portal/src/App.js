519ac4bda57d62bac10d23b0cb6a80d643629531
Chamodi Indrejith <104416340+chamoindrejith@users.noreply.github.com>
2024-11-19 17:50:31 +0000

    Add fix Profile picture and alignments

--- student_information_portal/src/App.js
+++ student_information_portal/src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import '../assets/css/compo.css'
+import './assets/css/compo.css'
 import { students } from './data/studentdata'
 import { useState } from 'react'
 import Profile from './components/Profile'
@@ -8,20 +7,17 @@ import DetailsTable from './components/DetailsTable'
 function App() {
   const [studentIndex, setStudentIndex] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Students Information Portal</h1>
-        <div className="outerDiv">
-          <div className="rightDiv">
-            <Profile student={students[studentIndex]} />
-          </div>
+    <>
+      <center><h3>Students Information Portal</h3></center>
+      <div className="outerDiv">
+        <div className="rightDiv">
+          <Profile student={students[studentIndex]} />
         </div>
-
-        <div className='leftDiv'>
-          <DetailsTable students={students} setStudentIndex={setStudentIndex}/>
+        <div className="leftDiv">
+          <DetailsTable students={students} setStudentIndex={setStudentIndex} />
         </div>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
